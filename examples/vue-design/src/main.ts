a0b61c9b5c1254bbdb71cee03f4dbe0a275da2e4
LiHowe <howe.personal@gmail.com>
2022-05-05 17:48:42 +0800

    feat: add actions [WIP]

--- examples/vue-design/src/main.ts
+++ examples/vue-design/src/main.ts
@@ -14,27 +14,65 @@ let activeEffect
 const obj = new Proxy(data, {
   get(target, key) {
     if (!activeEffect) return
+    // 得到对象对应的 key - effects map
     let depsMap = bucket.get(target)
     if (!depsMap) bucket.set(target, (depsMap = new Map()))
+    // 得到对象对应key的effects set
     let deps = depsMap.get(key)
     if (!deps) depsMap.set(key, (deps = new Set()))
+    debugger
+    // 添加当前激活的副作用函数
     deps.add(activeEffect)
 
     return target[key]
   },
   set(target, key, value) {
+    // 赋值
     target[key] = value
+    // 获得对象对应的 key effects map
     const depsMap = bucket.get(target)
-    if (!depsMap) return
+    debugger
+    if (!depsMap) return true
+    // 取得对应key的副作用函数数组
     const effects = depsMap.get(key)
+    if (!effects) return true
     effects.forEach(fn => fn())
     return true
   }
 })
 
 function effect(fn) {
-  activeEffect = fn
-  fn()
+  const effectFn = () => {
+    cleanup(effectFn)
+    activeEffect = fn
+    fn()
+  }
+  // 用来保存当前副作用函数的依赖,用于后面将副作用函数与其关联的依赖解绑.
+  effectFn.deps = []
+  effectFn()
+}
+
+// 用于 副作用函数与对象重新建立联系, 并清除旧联系来避免多余的副作用函数触发.
+function cleanup(fn) {
+  for (let i = 0; i < fn.deps.length; i++) {
+    const deps = fn.deps[i]
+    // 将副作用函数从依赖集合中移除
+    deps.delete(fn)
+  }
+  // 清空副作用函数依赖
+  fn.deps.length = 0
+}
+
+function track(target, key) {
+  if (!activeEffect) return
+  let depsMap = bucket.get(target)
+  if (!depsMap) bucket.set(target, (depsMap = new Map()))
+  let deps = depsMap.get(key)
+  if (!deps) depsMap.set(key, (deps = new Set()))
+  deps.add(activeEffect)
+  // 为了与副作用数组建立联系
+  activeEffect.deps.push(deps)
+
 }
 
 effect(() => {
@@ -44,6 +82,17 @@ effect(() => {
 const btn = document.createElement('button')
 btn.addEventListener('click', () => {
   obj.text = `Hello ${Math.round(Math.random() * 10)}`
+  // obj.flag = !obj.flag
 })
 btn.innerHTML = 'Change Text'
 document.body.appendChild(btn)
+
+
+function traverse(value, seen = new Set()) {
+  if (typeof value !== 'object' || value === null || seen.has(value)) return
+  seen.add(value)
+  for(const key in value) {
+    traverse(value[key], seen)
+  }
+  return value
+}
